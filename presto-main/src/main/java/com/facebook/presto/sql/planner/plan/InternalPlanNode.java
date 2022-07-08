d48717d42df6cd3ad437177a1dc93d18fa99cbe2
Sergey Smirnov <smirnoff@fb.com>
2022-07-07 21:40:47 -0700

    disabled instanceof check as it gets broken for multiple classloaders case

--- presto-main/src/main/java/com/facebook/presto/sql/planner/plan/InternalPlanNode.java
+++ presto-main/src/main/java/com/facebook/presto/sql/planner/plan/InternalPlanNode.java
@@ -20,8 +20,6 @@ import com.facebook.presto.spi.plan.PlanVisitor;
 
 import java.util.Optional;
 
-import static com.google.common.base.Preconditions.checkArgument;
-
 public abstract class InternalPlanNode
         extends PlanNode
 {
@@ -32,7 +30,8 @@ public abstract class InternalPlanNode
 
     public final <R, C> R accept(PlanVisitor<R, C> visitor, C context)
     {
-        checkArgument(visitor instanceof InternalPlanVisitor, "PlanVisitor is only for connector to use; InternalPlanNode should never use it");
+//        Disable to avoid class loader issues (instanceof is false for instance of the same class loaded by different classloader)
+//        checkArgument(visitor instanceof InternalPlanVisitor, "PlanVisitor is only for connector to use; InternalPlanNode should never use it");
         return accept((InternalPlanVisitor<R, C>) visitor, context);
     }
 
